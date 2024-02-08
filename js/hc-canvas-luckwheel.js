2134c3d57baa2290f6f53fd4a24d1c8f5dc08f0f
Thien Huynh <thnhuynhngoc@gmail.com>
2024-02-09 04:08:42 +0700

    v1

--- js/hc-canvas-luckwheel.js
+++ js/hc-canvas-luckwheel.js
@@ -127,7 +127,7 @@
       html.push(transform + ": rotate(" + i * turnNum + 'turn)">');
       if (opts.mode == "both") {
         html.push("<p id='curve'>" + prizeList[i].text + "</p>");
-        html.push('<img src="' + prizeList[i].img + '" />');
+        html.push('<img onclick="editPrize()" src="' + prizeList[i].img + '" />');
       } else if (prizeList[i].img) {
         html.push('<img src="' + prizeList[i].img + '" />');
       } else {
@@ -263,3 +263,33 @@
     });
   }
 })();
+
+// function clickedClassHandler(name,callback) {
+
+//   // apply click handler to all elements with matching className
+//   var allElements = document.body.getElementsByTagName("*");
+
+//   for(var x = 0, len = allElements.length; x < len; x++) {
+//       if(allElements[x].className == name) {
+//           allElements[x].onclick = handleClick;
+//       }
+//   }
+
+//   function handleClick() {
+//       var elmParent = this.parentNode;
+//       var parentChilds = elmParent.childNodes;
+//       var index = 0;
+
+//       for(var x = 0; x < parentChilds.length; x++) {
+//           if(parentChilds[x] == this) {
+//               break;
+//           }
+
+//           if(parentChilds[x].className == name) {
+//               index++;
+//           }
+//       }
+
+//       callback.call(this,index);
+//   }
+// }
